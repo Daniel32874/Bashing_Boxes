e5e263bfc1303dd1326a5bf1a11418006b7eaab5
Ears <ears@EarsfrHost.myguest.virtualbox.org>
2025-10-15 15:22:15 -0500

    fixed error by adding extra space

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
  word=( "Milkshake" "Magazine" "Notebook" "Waffle iron" "Bottle opener" "Barley" "Cassette tape" "Accordion" "Bus" "Cucumber")
-echo"${word[@]}"
+echo "${word[@]}"
 
 read -p "please pick a number between 0-9: " pos
 echo "item at $pos is: ${word[$pos]} "
