5d9b5bf2bb5f928d6dd52ce465ed3a91e13da127
Ears <ears@EarsfrHost.myguest.virtualbox.org>
2025-10-17 14:32:09 -0500

    Added a second function and cleaned up the code

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,6 +1,30 @@
  #!/bin/bash
- word=( "Milkshake" "Magazine" "Notebook" "Waffle iron" "Bottle opener" "Barley" "Cassette tape" "Accordion" "Bus" "Cucumber")
-echo "${word[@]}"
+ array_of_objects=( "Milkshake" "Magazine" "Notebook" "Waffle iron" "Bottle opener" "Barley" "Cassette tape" "Accordion" "Bus" "Cucumber")
+flag=false
 
-read -p "please pick a number between 0-9: " pos
-echo "item at $pos is: ${word[$pos]} "
+print_all_objects(){
+	echo "The list is as follow below:"
+	echo "${array_of_objects[@]}"
+	flag=true
+}
+
+print_all_objects
+
+print_object_at_posistion(){
+	
+	if [ $flag = true ]; then
+	read -p "please pick a number between 0-9: " posistion
+	echo "item at $posistion is: ${array_of_objects[$posistion]} "
+	fi
+}
+
+print_object_at_posistion
+
+add_new_item(){
+	read -p "Would you like to add a new object to the list?" answer
+	if [ $answer = yes ]; then
+	echo "the updated list is now as follow: ${array_of_objects + $answer} "
+	else [ $answer = no ]; then
+		stop
+}
+add_new_item
