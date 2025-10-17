6a8d7b339040d67a9e7efbb3e5c204f328dd7ca2
Ears <ears@EarsfrHost.myguest.virtualbox.org>
2025-10-17 15:24:49 -0500

    Added a third function and cleaned up the code

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -12,7 +12,7 @@ print_all_objects
 
 print_object_at_posistion(){
 	
-	if [ $flag = true ]; then
+	if [ "$flag" = "true" ]; then
 	read -p "please pick a number between 0-9: " posistion
 	echo "item at $posistion is: ${array_of_objects[$posistion]} "
 	fi
@@ -21,10 +21,26 @@ print_object_at_posistion(){
 print_object_at_posistion
 
 add_new_item(){
-	read -p "Would you like to add a new object to the list?" answer
-	if [ $answer = yes ]; then
-	echo "the updated list is now as follow: ${array_of_objects + $answer} "
-	else [ $answer = no ]; then
-		stop
+	read -p "Please add your desired word to the list " answer
+	array_of_objects+=("$answer")
+	echo "The updated list is now: ${array_of_objects[@]}"
+	#read -p "Would you like to add a new object to the list? yes/no" answer
+	#if [ "$answer" = "yes" ]; then
+		#echo "Please type in your desired below: " response
+		#echo "The updated list is now: ${array_of_objects[@]}"
+	#elif [ "$answer" = "no" ]; then
+		#exit
+	#fi
 }
+
 add_new_item
+
+delete_last_item(){
+	read -p "Would you like to add a new object to the list? yes/no" answer
+	#if [ "$answer" = "yes" ]; then
+		#echo "Please type in your desired below: " response
+		#echo "The updated list is now: ${array_of_objects[@]}"
+	#elif [ "$answer" = "no" ]; then
+		#exit
+	#fi
+}
