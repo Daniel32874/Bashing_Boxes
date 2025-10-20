8390b04ed706895e9e581078e8da9692cbbd71e3
Ears <ears@EarsfrHost.myguest.virtualbox.org>
2025-10-20 15:21:41 -0500

    I added function 6 and menu

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,7 +1,18 @@
  #!/bin/bash
  array_of_objects=( "Milkshake" "Magazine" "Notebook" "Waffle iron" "Bottle opener" "Barley" "Cassette tape" "Accordion" "Bus" "Cucumber")
 flag=false
+echo "1. View all items"
+    echo "2. View an item by position"
+    echo "3. Add a new word"
+    echo "4. Delete the last word"
+    echo "5. Delete a word by position"
+    echo "6. Exit"
+    read -p "Choose an option (1-6): " function_choice
+    echo
 
+   case  $function_choice in
+
+1)
 print_all_objects(){
 	echo "The list is as follow below:"
 	echo "${array_of_objects[@]}"
@@ -9,16 +20,20 @@ print_all_objects(){
 }
 
 print_all_objects
+;;
 
-print_object_at_posistion(){
+2)
+print_object_at_position(){
 	
 	if [ "$flag" = "true" ]; then
-	read -p "please pick a number between 0-9: " posistion
-	echo "item at $posistion is: ${array_of_objects[$posistion]} "
+	read -p "please pick a number between 0-9: " position
+	echo "item at $position is: ${array_of_objects[$position]} "
 	fi
 }
+;;
 
-print_object_at_posistion
+3)
+print_object_at_position
 
 add_new_item(){
 	read -p "Please add your desired word to the list: " answer
@@ -27,17 +42,30 @@ add_new_item(){
 }
 
 add_new_item
+;;
 
+4)
 remove_last_item(){
 	unset 'array_of_objects[-1]'
 	echo "Your desired word has been removed"
 	echo "The updated list is now: ${array_of_objects[@]}"
 }
 remove_last_item
+;;
 
+5)
 delete_item(){
-	read -p "Please type in the world you would like to delete below: " response
+	read -p "Please type in the position(number) you would like to delete below: " response
 	unset "array_of_objects[$response]"
 	echo "The updated list is now: ${array_of_objects[@]}"
 }
 delete_item
+;;
+
+6)
+end_script(){
+	exit 0
+}
+end_script
+;;
+esac
