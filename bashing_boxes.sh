fa1bb7d71d2d5ecbdc2ef4fa02724d7d3f0f8406
Ears <ears@EarsfrHost.myguest.virtualbox.org>
2025-10-22 15:19:23 -0500

    I made the code  nicer and more organized

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
  array_of_objects=( "Milkshake" "Magazine" "Notebook" "Waffle iron" "Bottle opener" "Barley" "Cassette tape" "Accordion" "Bus" "Cucumber")
 
- display_menu_options(){
-echo "1. View all items"
+display_menu_options(){
+	echo "1. View all items"
     echo "2. Print word at position"
     echo "3. Add a new word"
     echo "4. Delete the last word"
@@ -10,61 +10,80 @@ echo "1. View all items"
     echo "6. Exit"
     read -p "Choose an option (1-6): " function_choice
     echo
+    check_user_input
 }
-display_menu_options
 
-   case  $function_choice in
 
-1)
+check_user_input(){
+	case  $function_choice in
+
+		1)
+
+		print_all_objects
+		;;
+
+		2)
+
+		print_object_at_position
+		;;
+
+		3)
+
+		add_new_item
+		;;
+
+		4)
+
+		remove_last_item
+		;;
+
+		5)
+
+		delete_item
+		;;
+
+		6)
+
+		end_script
+		;;
+
+		*)
+		display_menu_options
+	    ;;
+	esac
+
+		
+}
+
 print_all_objects(){
 	echo "The list is as follow below:"
 	echo "${array_of_objects[@]}"
+	display_menu_options
 }
-
-print_all_objects
-;;
-
-2)
 print_object_at_position() {
 	read -p "please pick a number between 0-9: " position
 	echo "item at $position is: ${array_of_objects[$position]} "
+	display_menu_options
 }
-
-print_object_at_position
-;;
-
-3)
 add_new_item(){
 	read -p "Please add your desired word to the list: " answer
 	array_of_objects+=("$answer")
 	echo "The updated list is now: ${array_of_objects[@]}"
+	display_menu_options
 }
-
-add_new_item
-;;
-
-4)
 remove_last_item(){
 	unset 'array_of_objects[-1]'
 	echo "Your desired word has been removed"
 	echo "The updated list is now: ${array_of_objects[@]}"
+	display_menu_options
 }
-remove_last_item
-;;
-
-5)
 delete_item(){
 	read -p "Please type in the position(number) you would like to delete below: " response
 	unset "array_of_objects[$response]"
 	echo "The updated list is now: ${array_of_objects[@]}"
+	display_menu_options
 }
-delete_item
-;;
-
-6)
 end_script(){
 	exit 0
-}
-end_script
-;;
-esac
+} 
+display_menu_options
